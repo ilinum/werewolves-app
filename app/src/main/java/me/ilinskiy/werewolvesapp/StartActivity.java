3be0dba8e5d8efe98dfe113407718bc5e91accf3
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2016-09-15 23:54:05 -0500

    Add basic files

---
+++ app/src/main/java/me/ilinskiy/werewolvesapp/StartActivity.java
@@ -0,0 +1,13 @@
+package me.ilinskiy.werewolvesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
